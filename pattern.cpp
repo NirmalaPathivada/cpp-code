ea5d7f1b595667dc5b7c55dceb2f84b342ed695f
NirmalaPathivada <143801923+NirmalaPathivada@users.noreply.github.com>
2023-10-12 18:12:02 +0530

    Add files via upload

--- pattern.cpp
+++ pattern.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n=3;
+    int n=5;
     for(int j=0;j<5;j++){
     for(int i=0;i<=n;i++){
         
     cout<<"*";
     }
     cout<<"*"<<endl;
+    }
+
+    for(int k=1;k<=n;k++){
+        for(int i=0;i<=k-1;i++){
+            cout<<"*";
+        }
+        cout<<endl;
+
     }
  return 0;
 }
